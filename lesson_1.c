44e8a8f19336f2d90fb823c108c244e04e073120
Jason <861317026.@qq.com>
2017-01-11 08:40:13 +0800

    lesson_1.c APUD

---
+++ lesson_1.c
@@ -0,0 +1,8 @@
+// 第一节 APDU指令
+
+/*		在 git remote add jason git@github.com:MakeLinuxYeah/CPUCard.git时
+ *	出现错误：fatal: Not a git repository (or any of the parent directories): .git
+ *	只需要git init一下，接着再次执行即可。
+ */
+
+
